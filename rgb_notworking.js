e7463336efc2bca8b02afbadd508e048c8e82a90
machynist <senojderf@gmail.com>
2020-04-04 16:45:00 -0700

    auto

--- rgb_notworking.js
+++ rgb_notworking.js
@@ -5,13 +5,10 @@
     let connectButton = document.querySelector("#connect");
     let getdocButton = document.querySelector("#getdoc");
     let statusDisplay = document.querySelector('#status');
-    let redSlider = document.querySelector('#red');
-    let greenSlider = document.querySelector('#green');
-    let blueSlider = document.querySelector('#blue');
     let ztdoc = document.querySelector('#ZTDoc');
     let port;
 
-    getdocButton.textContent = '2';
+    getdocButton.textContent = '1';
 
     function connect() {
       port.connect().then(() => {
@@ -23,6 +20,7 @@
         port.onReceive = data => {
           let textDecoder = new TextDecoder();
           console.log(textDecoder.decode(data));
+          ztdoc.innerHTML += textDecoder.decode(data);
         }
         port.onReceiveError = error => {
           console.error(error);
@@ -30,27 +28,7 @@
       }, error => {
         statusDisplay.textContent = error;
       });
-    }
-
-    function onUpdate() {
-      if (!port) {
-        return;
-      }
-
-      /*
-      let view = new Uint8Array(3);
-      view[0] = parseInt(redSlider.value);
-      view[1] = parseInt(greenSlider.value);
-      view[2] = parseInt(blueSlider.value);
-      port.send(view);
-      */
-      port.send('G');
-
     };
-
-    redSlider.addEventListener('input', onUpdate);
-    greenSlider.addEventListener('input', onUpdate);
-    blueSlider.addEventListener('input', onUpdate);
 
     connectButton.addEventListener('click', function() {
       if (port) {
@@ -67,11 +45,11 @@
         });
       }
     });
-    
+
     getdocButton.addEventListener('click', function() {
-      getdocButton.textContent = 'working';
-      //port.send('G');
-    });
+        getdocButton.textContent = 'working';
+        //port.send('G');
+      });
 
     serial.getPorts().then(ports => {
       if (ports.length == 0) {
